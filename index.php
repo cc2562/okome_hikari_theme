d16ed7f000be775b7a2efea6ef133b3c43f3e308
CCRICE <2371092140@qq.com>
2025-12-20 08:18:33 +0800

    更新版本为1.0.6

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * okome hikari稻曦主题
  * @package okome hikari
  * @author CC米饭
- * @version 1.0.5
+ * @version 1.0.6
  * @link https://github.com/cc2562/okome_hikari_theme
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
