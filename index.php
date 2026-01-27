42967bd5c90ee32016f20e2716e87fed7b498df1
CCRICE <2371092140@qq.com>
2026-01-27 22:07:01 +0800

    chore: Bump theme version to 1.0.7 and reformat `__TYPECHO_ROOT_DIR__` definition check.

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
  * okome hikari稻曦主题
  * @package okome hikari
  * @author CC米饭
- * @version 1.0.6
+ * @version 1.0.7
  * @link https://github.com/cc2562/okome_hikari_theme
  */
-if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+if (!defined('__TYPECHO_ROOT_DIR__'))
+    exit;
 Get::Components('AppHeader');
 Get::Components('Home');
 Get::Components('AppFooter');
